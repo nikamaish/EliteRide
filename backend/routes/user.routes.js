7752e3e159deda24038e6fdd9d6bb2b312bdb535
Aishwarya Nikam <aishnikam31@gmail.com>
2025-02-13 00:29:33 +0530

    :art: added register route with validation

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {body} = require('express-validator');
+const userController = require('../controllers/user.controller');
+
+
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid email'), // if not then msg invalid
+    body('fullname.firstName').isLength({min: 3}).withMessage('First name must be at least 3 characters long'),
+    body('password').isLength({min:5}).withMessage('Password must be at least 5 characters long'),
+])
+
+module.exports = router;
